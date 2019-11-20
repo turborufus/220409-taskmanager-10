9fb091ba6035767b15073f6799d4679b03797050
Alexandra Samonenko <turborufus@gmail.com>
2019-11-20 06:34:23 +0300

    smallfix

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+'use strict'; 
+
 const TASK_COUNT = 3;
 
 const createSiteMenuTemplate = () => {
@@ -472,7 +474,7 @@ render(taskListElement, createTaskEditTemplate(), `beforeend`);
 new Array(TASK_COUNT)
   .fill(``)
   .forEach(
-    () => render(taskListElement, createTaskTemplate(), `beforeend`)
+      () => render(taskListElement, createTaskTemplate(), `beforeend`)
   );
 
 const boardElement = siteMainElement.querySelector(`.board`);
